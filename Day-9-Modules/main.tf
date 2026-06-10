34b6062fd38ba0287d8f313e6c336ad192e416b3
edwinfrancislawrence <edwinfrancislawrence@gmail.com>
2026-06-10 19:33:24 +0530

    Modules testing

---
+++ Day-9-Modules/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  
+ 
+
+#   tags = merge(
+#     {
+#       Name = var.name
+#     },
+#     var.tags
+#   )
+}
