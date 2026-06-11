af764cf53d9a5d79cc97e0fc4cad69e13c962654
edwinfrancislawrence <edwinfrancislawrence@gmail.com>
2026-06-11 18:12:12 +0530

    Day 10 Module variables to local variables

---
+++ Day-10-Modules-Examples/main.tf
@@ -0,0 +1,14 @@
+module "s3_bucket" {
+    source = "github.com/edwinfrancislawrence/terraform-aws-s3-bucket.git"
+    
+    S3_name  = var.S3_name # 👈 Change to your globally unique name
+    
+
+  control_object_ownership = var.control_object_ownership
+  
+
+  versioning = {
+    enabled = var.versioning # 👈 Automatically enables version protection
+  }
+  
+}
---
+++ Day-10-Modules-Examples/variables.tf
@@ -0,0 +1,14 @@
+variable "S3_name" {
+    default = "edwintestingmodulebucket-2026"
+  
+}
+
+
+variable "control_object_ownership" {
+    default     = "true"
+}
+
+variable "versioning" {
+    default = "true"
+  
+}
